69719fe2a084497dd705ad7a4afd4ecf3052bce3
JanOlaf1 <122294366+JanOlaf1@users.noreply.github.com>
2024-10-18 14:45:43 +0300

    lisätty firebase

---
+++ src/(firebase)/index.jsx
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2yaB4j2g-GlDGhUUMaFYnonIxuVHEOd0",
+  authDomain: "suomen-ilmailuliito.firebaseapp.com",
+  projectId: "suomen-ilmailuliito",
+  storageBucket: "suomen-ilmailuliito.appspot.com",
+  messagingSenderId: "1024518890397",
+  appId: "1:1024518890397:web:b18f586f282218ce130574"
+};
+// Initialize Firebase
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+// Initialize Firestore
+export const firestore = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
+
+// Initialize Analytics
+export const analytics = (async () => {
+    if (typeof window !== 'undefined') {
+        const supported = await isSupported();
+        return supported ? getAnalytics(app) : null;
+    }
+    return null;
+})();
--- src/components/SaaEnnuste.jsx
+++ src/components/SaaEnnuste.jsx
@@ -87,7 +87,11 @@ const SaaEnnuste = () => {
         console.log('Extracted Data:', extractedData);
         setWeatherData(extractedData);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        if (error.name === 'AbortError') {
+          console.error('Fetch aborted due to timeout');
+        } else {
+          console.error('Error fetching data:', error);
+        }
       } finally {
         setLoading(false);
       }
@@ -158,8 +162,8 @@ const SaaEnnuste = () => {
                     {infoVisible['mobiili'] && (
                       <div 
                         ref={infoRef} 
-                        className={`absolute left-1/2 transform -translate-x-[calc(50%+20px)] mt-1 bg-white border border-gray-300 p-2 rounded shadow-lg z-10`}
-                        >
+className={`absolute left-1/2 transform -translate-x-[calc(50%+20px)] mt-1 bg-white border border-gray-300 p-2 rounded shadow-lg z-10`}
+                      >
                         <p>{info['mobiili']}</p>
                         <FaTimes
                           className="absolute top-1 right-1 text-red-500 cursor-pointer"
